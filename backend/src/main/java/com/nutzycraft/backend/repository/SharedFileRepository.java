1af1dcf96f43226788874e8ea86db986b532ae06
Soesh Cooray <soeshcooray@gmail.com>
2025-12-23 23:52:50 +0530

    Deleted previous configs for handling images

--- backend/src/main/java/com/nutzycraft/backend/repository/SharedFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nutzycraft.backend.repository;
-
-import com.nutzycraft.backend.entity.SharedFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SharedFileRepository extends JpaRepository<SharedFile, Long> {
-    List<SharedFile> findByJobIdOrderByUploadedAtDesc(Long jobId);
-}
